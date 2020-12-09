d592e0537d11989470dea8785bccfff3ecf4cd80
algebrabender <jelena.firulovic@gmail.com>
2020-12-09 13:17:47 +0100

    update 9.12

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ import {Katalog} from "./katalog.js"
 
 const katalog1 = new Katalog("GameStop", 3, 5, 10);
 katalog1.crtajKatalog(document.body);
+
+const katalog2 = new Katalog("GameCentar", 2, 6, 15);
+katalog2.crtajKatalog(document.body);
